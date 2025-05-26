6d13cf8f9b6f140904c2b3683e91add165d73d96
Danillo Gomes <danillog821@gmail.com>
2025-05-26 08:17:59 -0300

    feat/polished-movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,15 @@ extends CharacterBody2D
 @export var limit_left: float = -400.0
 @export var limit_right: float = 400.0
 
+@export var max_speed: = 200.0
+@export var acceleration: = 1000.0
+@export var friction: = 1000.0
+@export var air_friction: = 2000.0
+@export var air_acceleration: = 500.0
+@export var up_gravity: = 980.0
+@export var down_gravity: = 600.0
+@export var jump_amount: = 400.0
+
 var fall_start_position: float = 0.0
 var was_on_floor: bool = true
 
@@ -24,14 +33,13 @@ func _ready() -> void:
 	)
 
 func _physics_process(delta: float) -> void:
-	var x_input = Input.get_axis("ui_left", "ui_right")
+	var x_input = Input.get_axis("move_left", "move_right")
 	
 	# DETECTA INÍCIO DA QUEDA
 	if was_on_floor and not is_on_floor():
 		fall_start_position = global_position.y
 	
-	if not is_on_floor():
-		velocity.y += 980 * delta
+	apply_gravity(delta)
 	
 	if not was_on_floor and is_on_floor():
 		var fall_height = global_position.y - fall_start_position
@@ -39,17 +47,17 @@ func _physics_process(delta: float) -> void:
 	
 	was_on_floor = is_on_floor()
 	
-	if Input.is_action_just_pressed("ui_accept") or Input.is_joy_button_pressed(0, JOY_BUTTON_A):
+	if Input.is_action_just_pressed("attack"):
 		animation_player_upper.play("attack")
 	
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y -= 400
-	
-	velocity.x = x_input * 80
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_amount
 	
 	if(x_input == 0):
+		apply_friction(delta)
 		animation_player_lower.play("idle")
 	else:
+		accelerate_horizontally(x_input, delta)
 		anchor.scale.x = sign(x_input)
 		animation_player_lower.play("run")
 	
@@ -57,6 +65,25 @@ func _physics_process(delta: float) -> void:
 		animation_player_lower.play("jump")
 	
 	move_and_slide()
+
+func accelerate_horizontally(horizontal_direction: float, delta: float) -> void:
+	var acceleration_amount = acceleration
+	if not is_on_floor(): acceleration_amount = air_acceleration
+	velocity.x = move_toward(velocity.x, max_speed * horizontal_direction, acceleration_amount * delta * abs(horizontal_direction))
+	pass
+
+func apply_friction(delta) -> void:
+	var friction_amount: = friction
+	if not is_on_floor(): friction_amount = air_friction
+	velocity.x = move_toward(velocity.x, 0.0, friction_amount * delta)
+	pass
+
+func apply_gravity(delta) -> void:
+	if not is_on_floor(): 
+		if velocity.y <= 0.0:
+			velocity.y += up_gravity * delta
+		else:
+			velocity.y += down_gravity * delta
 
 func check_fall_damage(fall_height: float) -> void:
 	if fall_height >= 100.0:
@@ -81,7 +108,7 @@ func apply_landing_vibration() -> void:
 		var duration = 0.1
 		
 		Input.start_joy_vibration(0, intensity, intensity, duration)
-		#Input.start_joy_vibration(0, 0.3, 0.3, 0.1)
+		#Input.start_joy_vibration(0, 0.3, 0.3, 0.1)aa
 
 func _process(delta: float) -> void:
 	if Input.get_connected_joypads().size() > 0:
